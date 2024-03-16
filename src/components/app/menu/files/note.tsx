dbd666db9ff901aebfc020a29232a6de643656e4
Lorre Huggan <lorre.huggan@protonmail.com>
2024-03-16 18:40:03 +0000

    Revert "refactor(core): Streamline text editor functionality"

--- src/components/app/menu/files/note.tsx
+++ src/components/app/menu/files/note.tsx
@@ -4,13 +4,15 @@ import {
   ContextMenuItem,
   ContextMenuTrigger,
 } from "@/components/ui/context-menu";
+import { deleteNote } from "@/lib/actions/editor";
 import type { Note } from "@/lib/db/schema/note";
 import { FileText } from "lucide-react";
 
 type Props = {
   note: Note;
 };
-export default function Note({ note }: Props) {
+export default async function Note({ note }: Props) {
+  const dn = deleteNote.bind(null, { id: note.id });
   return (
     <ContextMenu>
       <ContextMenuTrigger>
@@ -22,7 +24,11 @@ export default function Note({ note }: Props) {
         </li>
       </ContextMenuTrigger>
       <ContextMenuContent>
-        <ContextMenuItem>Delete</ContextMenuItem>
+        <ContextMenuItem>
+          <form action={dn}>
+            <button type="submit">delete</button>
+          </form>
+        </ContextMenuItem>
       </ContextMenuContent>
     </ContextMenu>
   );
--- src/components/app/menu/index.tsx
+++ src/components/app/menu/index.tsx
@@ -2,6 +2,7 @@ import Folder from "@/components/app/menu/files/folder";
 import Note from "@/components/app/menu/files/note";
 import { db } from "@/lib/db";
 import { folderTable, noteTable } from "@/lib/db/schema/note";
+import Nav from "./nav";
 
 export default async function Menu() {
   const notes = await db.select().from(noteTable);
@@ -9,9 +10,11 @@ export default async function Menu() {
 
   return (
     <section className="col-span-2 grid grid-cols-4 border-r-[1px] border-muted-foreground/20">
-      <div className="border-r-[1px] border-muted-foreground/20 px-4">Menu</div>
+      <div className="border-r-[1px] pt-4 border-muted-foreground/20 px-4 flex flex-col items-center">
+        <Nav />
+      </div>
       <div className="col-span-3 px-4 text-sm pt-4">
-        <ul className="space-y-1">
+        <ul className="flex flex-col gap-1">
           <li>
             <a href="/app/editor">Go To Editor</a>
           </li>

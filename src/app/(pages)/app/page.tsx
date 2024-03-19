a48a4001277475b38150522c10055eae4adec721
Lorre <lorre.huggan@protonmail.com>
2024-03-19 01:37:26 +0000

    feat: Update App component UI and functionality
    - Updated the App component to include a new UI layout and structure
    - Changed the return redirect path in deleteNote, trashNote, restoreNote,
      and updateNoteFolder functions to "/app" instead of "/app/editor"
    - Refactored the App component to include database insertion functionality
    - Added logic to create a new note with a default title and content
    - Redirect the user to the newly created note's editor page
    
    These changes were made to enhance the user experience and improve the overall functionality of the application.

--- src/app/(pages)/app/page.tsx
+++ src/app/(pages)/app/page.tsx
@@ -1,7 +1,9 @@
 export default function App() {
   return (
-    <section className="col-span-9">
-      <p>App</p>
+    <section className="col-span-9 p-4">
+      <div className="prose">
+        <h1 className="">App:</h1>
+      </div>
     </section>
   );
 }

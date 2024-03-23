a6bdea9360db6b319ab144186f08cbeb7aa674d4
Lorre <lorre.huggan@protonmail.com>
2024-03-23 22:07:32 +0000

    refactor: revise table structures and add new columns
    Adjust table structures for 'folder', 'note', and 'user' tables.
    Add the 'user_id' column to the 'folder' table.
    Introduce the 'trashed' column with a default value of false to the 'note' table.
    Include indexes on primary keys and foreign keys in all three tables.

--- src/lib/db/schema/user.ts
+++ src/lib/db/schema/user.ts
@@ -1,14 +1,22 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { index, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
 
-export const userTable = sqliteTable("user", {
-  id: text("id").notNull().primaryKey().unique(),
-  name: text("name"),
-  email: text("email").notNull(),
-  createdAt: text("created_at").notNull().default(Date.now().toLocaleString()),
-  updateAt: text("updated_at").notNull().default(Date.now().toLocaleString()),
-});
+export const userTable = sqliteTable(
+  "user",
+  {
+    id: text("id").notNull().primaryKey().unique(),
+    name: text("name"),
+    email: text("email").notNull(),
+    createdAt: text("created_at").notNull().default(new Date().toISOString()),
+    updateAt: text("updated_at").notNull().default(new Date().toISOString()),
+  },
+  (table) => {
+    return {
+      idIdx: index("user_id_idx").on(table.id),
+    };
+  },
+);
 
 export const insertUserSchema = createInsertSchema(userTable);
 export const selectUserSchema = createSelectSchema(userTable);
